9c4e2f2aeeb0478005698ac6864c04e9d6a840f8
raymondkietchau <raymondkietchau@gmail.com>
2017-08-14 16:01:19 -0700

    A quick excercise on day 1.

---
+++ 2-Variables/main.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+
+int main()
+{
+	/*
+	Because I'm too lazy to go through all this.
+
+	int thatOneVariable = 9;
+	std::cout << thatOneVariable << std::endl;
+	thatOneVariable = 12;
+	std::cout << thatOneVariable << std::endl;
+	thatOneVariable = 7;
+
+	while (thatOneVariable >= 0)
+	{
+		std::cout << thatOneVariable-- << std::endl;
+	}
+	system("pause");
+
+	int numberOne;
+	int numberTwo;
+
+	std::cout << "\nChoose two numbers to add, pressing Enter after each number.\n";
+	std::cin >> numberOne >> numberTwo;
+	int numberThree = numberOne + numberTwo;
+	std::cout << "Those two numbers equals " << numberThree << "!" << std::endl;
+	system("pause");
+
+	float degCelsius = 78.0f;
+	float degFahrenheit = 0.0f;
+
+	degFahrenheit = degCelsius * 1.8f + 32;
+
+	std::cout << "\nCelcius to Fahrenheit)" << std::endl;
+	std::cout << "Celcius:	" << degCelsius << std::endl;
+	std::cout << "Fahrenheit:	" << degFahrenheit << std::endl;
+	system("pause");
+
+	float degCelsiusUser;
+
+	std::cout << "\nPlease enter a Celsius value to convert to Fahrenheit." << std::endl;
+	std::cin >> degCelsiusUser;
+
+	degFahrenheit = degCelsiusUser * 1.8f + 32;
+	std::cout << degCelsiusUser << " Celsius is " << degFahrenheit << " Fahrenheit." << std::endl;
+	system("pause");
+
+	float rectHeight = 0.0f;
+	float rectWidth = 0.0f;
+	float rectArea = 0.0f;
+
+	std::cout << "\nFind the area of a rectangle! Please provide a height." << std::endl;
+	std::cin >> rectHeight;
+	std::cout << "Now please provide a width." << std::endl;
+	std::cin >> rectWidth;
+
+	rectArea = rectHeight * rectWidth;
+	std::cout << rectArea << "^2 units is the area of a rectangle " << rectHeight << " units high by " << rectWidth << " units wide." << std::endl;
+	system("pause");
+
+	float a, b, c, d, e;
+	float avg;
+	avg = a = b = c = d = e = 0.0f;
+
+	std::cout << "\nNow, apparently, we're gonna find the average of 5 numbers. Input them now:\n";
+	std::cin >> a >> b >> c >> d >> e;
+
+	avg = (a + b + c + d + e) / 5;
+	std::cout << "The average of those numbers is: " << avg << std::endl;
+	system("pause");
+
+	float numOne = 0.0f;
+	float numTwo = 0.0f;
+
+	std::cout << "\nOKAY. So, we're gonna swap some numbers now! Please enter two numbers to swap!" << std::endl;
+	std::cin >> numOne >> numTwo;
+
+	numOne = numOne + numTwo;
+	numTwo = numOne - numTwo;
+	numOne = numOne - numTwo;
+
+	std::cout << "First number is now " << numOne << " and the second number is now " << numTwo << std::endl;
+	system("pause");
+	*/
+
+	int age;
+
+	std::cout << "Howdy! How old are you?" << std::endl;
+	std::cin >> age;
+	std::cout << "Wow! You're " << age << " years old?" << std::endl;
+	std::cout << "You're only expected to live " << 84 - age << " years more in Japan, " << 79 - age << " years more in the US, and only " << 50 - age << " if you live in Sierra Leone!" << std::endl;
+	std::cout << "How do you feel about you inevitable demise creeping ever close with every breath you take? Can you feel every sin you've committed?" << std::endl;
+	system("pause");
+
+	/*
+	Misc Renaming of Variables
+
+	int age;
+	int bullet_count;
+	float dog_years;
+	int qty;
+	float shield_value;
+	float defense_matrix_cooldown;
+	int red_armor_value;
+	float red_armor_ratio;
+	int happiness;
+	int gandhi_aggression;
+	*/
+
+	/*
+	Listing Variables (~10)
+
+	bool num - Stores only True or False
+	float num - single precision floating point (7 decimals)
+	double num - double precision floating point (15 decimals)
+	char num - A single octet (one byte, 8 bits). One character of ASCII?
+	short int num - Not smaller than char, at least 16 bits.
+	int num - Integer. not smaller than short, at least 16 bits.
+	long int num - not smaller than int, at least 32 bits.
+	long long int num - not smaller than long, at least 64 bits.
+
+	unsigned char
+	unsigned int - Both same size as their signed counterparts, no negative numbers.
+
+	void num - Represents the absense of type
+	
+
+	*/
+
+}
