87abe493cdb4d7a9aa1c403084ce871347caa137
Raymond Chau <raymondkietchau@gmail.com>
2017-08-21 14:47:45 -0700

    Started work on 5-MultiFileProjects

---
+++ 5-MultiFileProjects/func.h
@@ -0,0 +1,4 @@
+#pragma once
+
+//returns the sum of the two integers.
+int sum(int a, int b);
---
+++ 5-MultiFileProjects/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "func.h"
+
+int main()
+{
+	int numA = 1;
+	int numB = 2;
+
+	std::cout << "Sum: " << sum(numA, numB) << std::endl;
+
+	system("pause");
+}
